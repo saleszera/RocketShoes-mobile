5acaed7c8982a46eb8b0cee598de08f9ecbd1a69
Sales <crowzcoleslaw@gmail.com>
2020-07-15 22:52:30 -0300

    estrutura basica do projeto, prettier, eslint e editorconfig já configurados

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Text>Funcionou porra!</Text>
+    </>
+  );
+}
