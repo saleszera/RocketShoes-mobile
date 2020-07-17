f3961569e5c70da73c0e03b75f15be0f887b48fa
Sales <crowzcoleslaw@gmail.com>
2020-07-16 21:40:22 -0300

    configurado redux e redux saga

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
+import store from './store';
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
