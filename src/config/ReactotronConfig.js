3cde73646b5f7c6abb2b9959dd065c3787ef65d8
Sales <crowzcoleslaw@gmail.com>
2020-07-16 21:41:18 -0300

    integrado redux e redux saga no reactotron

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,11 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure({ host: '192.168.11.199' })
+    .use(reactotronRedux())
+    .use(reactotronSaga())
     .useReactNative()
     .connect();
 
